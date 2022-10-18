0c20721439ccab1d8384c646327e4b82b2253b43
VigoFazal <frontendprofile@gmail.com>
2022-10-18 08:38:53 -0700

    v-02

--- js/javascript.js
+++ js/javascript.js
@@ -25,3 +25,37 @@ $( document ).ready(function() {
 
 });
 
+
+
+$(window).scroll(function() {
+	var a = 0;
+	var oTop = $('#counter').offset().top - window.innerHeight;
+	console.log($('#counter').offset().top);
+	if (a == 0 && $(window).scrollTop() > oTop) {
+	  $('.counter-value').each(function() {
+		var $this = $(this),
+		  countTo = $this.attr('data-count');
+		$({
+		  countNum: $this.text()
+		}).animate({
+			countNum: countTo
+		  },
+  
+		  {
+  
+			duration: 2000,
+			easing: 'swing',
+			step: function() {
+			  $this.text(Math.floor(this.countNum));
+			},
+			complete: function() {
+			  $this.text(this.countNum);
+			  //alert('finished');
+			}
+  
+		  });
+	  });
+	  a = 1;
+	}
+  
+  });
